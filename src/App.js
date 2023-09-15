a1cd1b9768ebc5f032b75af61ad31dca94d78f47
KaikySr <Kaikysr2004@gmail.com>
2023-09-15 11:40:09 -0300

    transcrevendo pdf

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import AddPage from "./pages/AddPage";
-import NotFoundPage from "./pages/NotFoundPage";
+import { AlertProvider } from "./context/alert";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+
+
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/add' element={<AddPage />} />
-      </Routes>
+      <AlertProvider>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/add' element={<AddPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+        </Routes>
+      </AlertProvider>
 
-      
       {/* <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
-        <Route path='/main' element={
+        <Route path='/main' element=
+        {
           <ProtectedRoute
             errorPage={<AccessDenied />}
             targetPage={<NavBar />}
           />
         }>
           <Route path='' element={<HomePage />} />
+          <Route path='/add' element={<AddPage />} />
         </Route>
         <Route path='*' element={<NotFoundPage />} />
       </Routes> */}
--- src/pages/AddPage/index.js
+++ src/pages/AddPage/index.js
@@ -1,20 +1,37 @@
 import styles from "./styles.module.scss"
+import Button from 'react-bootstrap/Button';
 
 export default function AddPage() {
     return (
         <div className={styles.main}>
-            <div className={styles.card}> 
-                <div>
-                    <h3>add post</h3>
-                </div>
+            <div className={styles.card}>
+
+                <b className={styles.addPost}>add post</b>
+
                 <div className={styles.inputTitle}>
-                    <input  placeholder="Title"></input>
+                    <span>Adicione um título para sua publicação:</span>
+                    <input placeholder="Título"></input>
                 </div>
 
                 <div className={styles.inputText}>
-                    <textarea></textarea>
+                    <textarea className={styles.textArea} placeholder="Compartilhe o que você esta pensando..."></textarea>
+                </div>
+
+                <div className={styles.buttonSubmit}>
+                    <Button variant="primary" type="submit">Publicar</Button>
                 </div>
             </div>
         </div>
     )
 }
+
+// import Formulario from "../../components/Formulario";
+// import NavBar from "../../components/navbar";
+// export default function AddPostPage() {
+//     return (
+//         <>
+//             <NavBar />
+//             <Formulario />
+//         </>
+//     )
+// }
